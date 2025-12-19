b923930d08437963d573714233afa9967dcf37bb
ISDNightNova <nightnovann@gmail.com>
2025-12-19 23:45:15 +0530

    Create basic.h

---
+++ src/basic.h
@@ -0,0 +1,16 @@
+#ifndef BASIC_H
+#define BASIC_H
+
+int sum(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+int divide(int x, int y);
+int mod(int x, int y);
+
+int fac(int x);
+double quo(int n);
+
+int ipow(int x, int y);
+double root(double x, double n);
+
+#endif // BASIC_H
